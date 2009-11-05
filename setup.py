76b9066fe6e20c2c3c5205506bc38510755ba1e2
sweh <sweh@localhost>
2009-11-05 11:43:00 +0000

    Pin version for gocept.collmex.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'setuptools',
-        'gocept.collmex',
+        'gocept.collmex>=0.7',
     ],
     zip_safe=False,
     entry_points = """
