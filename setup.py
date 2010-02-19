a7d648fe5e1f35e14bf473b9c870652d8f261017
ctheune <ctheune@localhost>
2010-02-19 10:36:52 +0000

    Preparing release 0.1.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ name = 'gocept.gtimelog'
 
 setup(
     name=name,
-    version = '0.1.3dev',
+    version = '0.1.3',
     license='ZPL 2.1',
     description='gocept fork of gtimelog.',
     long_description = (read('README.txt')),
