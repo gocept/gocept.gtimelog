1b0d068a4237669624b35c5f6c6b2293e107b11a
Christian Theune <ct@flyingcircus.io>
2010-02-26 09:46:00 +0000

    cleanup

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ import os.path
 
 from setuptools import setup, find_packages
 
+
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
@@ -14,15 +15,15 @@ name = 'gocept.gtimelog'
 
 setup(
     name=name,
-    version = '0.1.4dev',
+    version='0.1.4dev',
     license='ZPL 2.1',
     description='gocept fork of gtimelog.',
-    long_description = (read('README.txt')),
+    long_description=read('README.txt'),
     author='gocept gmbh & co. kg',
     author_email='sw@gocept.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    namespace_packages=['gocept',],
+    namespace_packages=['gocept'],
     include_package_data=True,
     install_requires=[
         'setuptools',
@@ -31,8 +32,7 @@ setup(
         'gocept.collmex>=0.7',
     ],
     zip_safe=False,
-    entry_points = """
+    entry_points="""
         [console_scripts]
         gtimelog = gocept.gtimelog.gtimelog:main
-    """,
-    )
+    """)
