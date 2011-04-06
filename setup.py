9b5b0bbdb3c691d8bed9dd497f871869310a97ca
wosc <wosc@localhost>
2011-04-06 11:00:35 +0000

    Preparing release 0.3.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ name = 'gocept.gtimelog'
 
 setup(
     name=name,
-    version='0.3.1dev',
+    version='0.3.1',
     license='ZPL 2.1',
     description='gocept fork of gtimelog.',
     long_description=read('README.txt'),
