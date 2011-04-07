188078ee6724ce9a55d69cc1e0a9c456fd2be876
Wolfgang Schnerring <wosc@wosc.de>
2011-04-07 07:52:30 +0000

    Back to development: 0.4.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ name = 'gocept.gtimelog'
 
 setup(
     name=name,
-    version='0.4.0',
+    version='0.4.1dev',
     license='ZPL 2.1',
     description='gocept fork of gtimelog.',
     long_description=read('README.txt'),
