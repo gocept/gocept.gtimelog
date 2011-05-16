bafc957601980bb264e195620f40e2ae90dd9712
wosc <wosc@localhost>
2011-05-16 06:33:03 +0000

    Preparing release 0.4.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ name = 'gocept.gtimelog'
 
 setup(
     name=name,
-    version='0.4.2dev',
+    version='0.4.2',
     license='ZPL 2.1',
     description='gocept fork of gtimelog.',
     long_description=read('README.txt'),
