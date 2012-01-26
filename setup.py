bc08543a46262336bcbcbaf8497b5cb9964c6511
Wolfgang Schnerring <wosc@wosc.de>
2012-01-26 07:03:30 +0000

    Preparing release 0.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(*rnames):
 
 setup(
     name='gocept.gtimelog',
-    version='0.4.3dev',
+    version='0.5',
     license='ZPL 2.1',
     description='gocept fork of gtimelog.',
     long_description=read('README.txt'),
