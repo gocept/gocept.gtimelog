812f4a99832b556e21fa5b674ed4936b31357fad
Michael Howitz <mh@gocept.com>
2012-02-06 17:08:21 +0100

    Back to development: 0.5.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(*rnames):
 
 setup(
     name='gocept.gtimelog',
-    version='0.5.1',
+    version='0.5.2dev',
     license='ZPL 2.1',
     description='gocept fork of gtimelog.',
     long_description=read('README.txt'),
