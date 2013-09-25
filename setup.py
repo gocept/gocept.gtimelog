b737c069041ad61af8ffb501f3ea24923c824123
Christian Zagrodnick <cz@gocept.com>
2013-09-25 08:26:07 +0200

    Preparing release 0.8.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(*rnames):
 
 setup(
     name='gocept.gtimelog',
-    version='0.8.2.dev0',
+    version='0.8.2',
     license='ZPL 2.1',
     description='gocept fork of gtimelog.',
     long_description=read('README.txt'),
