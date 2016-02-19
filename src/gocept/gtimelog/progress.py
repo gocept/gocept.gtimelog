1b077fb958e68660d4f979456bbe4069cd4058ba
Sebastian Wehrmann <sw@gocept.com>
2016-02-19 07:38:35 +0100

    Display progress of work done in current month and year in `gtl-progress` output.

--- src/gocept/gtimelog/progress.py
+++ src/gocept/gtimelog/progress.py
@@ -3,7 +3,7 @@
 # See also LICENSE.txt
 
 from __future__ import print_function
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from gocept.gtimelog.util import format_duration_long
 import argparse
 import gocept.gtimelog.cli
@@ -27,6 +27,35 @@ class WithoutColors(object):
 
     RED = ''
     BLACK = ''
+
+
+HOLIDAYS = [
+    date(2016, 1, 1),
+    date(2016, 1, 6),
+    date(2016, 3, 25),
+    date(2016, 3, 28),
+    date(2016, 5, 5),
+    date(2016, 5, 16),
+    date(2016, 10, 3),
+    date(2016, 10, 31),
+    date(2016, 12, 26),
+]
+
+
+def get_businessdays_until_now():
+    """Return amount of businessdays of current month until today.
+
+     This allows showing a progress over the current month and the
+     current year.
+    """
+
+    now = datetime.now()
+    businessdays = 0
+    for i in range(1, now.day + 1):
+        thisdate = date(now.year, now.month, i)
+        if thisdate.weekday() < 5 and thisdate not in HOLIDAYS:
+            businessdays += 1
+    return businessdays
 
 
 def main():
@@ -90,15 +119,17 @@ def main():
                          total_work.total_seconds())
     else:
         total_percent = 0
-    expected = 0
+    expected = progress_expected = 0
     engagement = settings.engagement
     if engagement:
         expected = engagement[today.month - 1]
+        progress_expected = int(get_businessdays_until_now() * settings.hours)
 
     print("Total work done this month: {colors.RED}{total_work} "
-          "({total_percent} %){colors.BLACK} of  {colors.RED}{expected} "
-          "hours{colors.BLACK}".format(
+          "({total_percent} %){colors.BLACK} of {colors.RED}{progress_expected}"
+          " ({expected}) hours{colors.BLACK}".format(
               colors=Colors,
+              progress_expected=progress_expected,
               expected=expected,
               total_work=format_duration_long(total_work),
               total_percent=round(total_percent, 1)))
@@ -111,12 +142,21 @@ def main():
 
     total_percent = (total_customer.total_seconds() * 100.0 /
                      total_work.total_seconds())
+    progress_engagement = 0
+    now = datetime.now()
+    for i in range(1, now.month + 1):
+        if i < now.month:
+            progress_engagement += settings.engagement[i - 1]
+        else:
+            progress_engagement += int(
+              get_businessdays_until_now() * settings.hours)
     engagement = sum(settings.engagement)
 
     print("Total work done this year:  {colors.RED}{total_work} "
-          "({total_percent} %){colors.BLACK} of {colors.RED}{expected} "
-          "hours{colors.BLACK}".format(
+          "({total_percent} %){colors.BLACK} of {colors.RED}{progress} "
+          "({expected}) hours{colors.BLACK}".format(
               colors=Colors,
+              progress=progress_engagement,
               expected=engagement,
               total_work=format_duration_long(total_work),
               total_percent=round(total_percent, 1)))
